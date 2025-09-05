8a4af43be454bb52d90263e1d4dfa27bf654e235
eMDeSo <dominoday222@gmail.com>
2025-09-05 22:39:28 +0200

    provided groups for player class and began asteroids

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updateable_group, drawable_group)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # game loop
@@ -16,7 +20,9 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+        updateable_group.update(dt)
+        for drawable in drawable_group:
+            drawable.draw(screen)
         pygame.display.flip()
         #limit frame rate to 60 fps
         dt = clock.tick(60)/1000
